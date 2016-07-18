cabecba8c023dc695a3b1283b92daaaaff8e33bb
thanhdt@viosoft <thanhdt@viosoft.com>
2016-07-18 15:09:20 +0700

    [U] Add sleeping time for schedule module after done each site and all

--- config/schedule.js
+++ config/schedule.js
@@ -2,10 +2,18 @@ module.exports = {
 	scenes: ['./config/chatvl.com', './config/gioitre.net', './config/haivn.com'],
 	//scenes: ['./config/chatvl.com'],
 	loop: -1,
-	sleep: () => {
-		let getRandomArbitrary = (min, max) => {
-	    return Math.random() * (max - min) + min;
-		};
-		return getRandomArbitrary(5, 50) * 200;
+	sleep: {
+		each: () => {
+			let getRandomArbitrary = (min, max) => {
+		    return Math.random() * (max - min) + min;
+			};
+			return getRandomArbitrary(5, 10) * 3 * 1000;
+		},
+		all: () => {
+			let getRandomArbitrary = (min, max) => {
+		    return Math.random() * (max - min) + min;
+			};
+			return getRandomArbitrary(10, 15) * 60 * 1000;
+		}
 	}
 }
--- schedule.js
+++ schedule.js
@@ -7,29 +7,30 @@ module.exports = (globalConfig, scheduleConfig) => {
 	for(var s of scheduleConfig.scenes){
 		scenes.push(new CrawlerBot(globalConfig, require(s)));
 	}	
-	let scan = (idx, loop) => {		
-		let scene = scenes[idx];		
-		let sleep = scheduleConfig.sleep();				
+	let scan = (idx, loop, sleep) => {		
+		let scene = scenes[idx];
 		if(loop > 0) console.log('|--R-E-P-E-A-T--[' + loop + ']');
 		else if(loop < 0) console.log('|--R-E-P-E-A-T---F-O-R-E-V-E-R');
 		console.log('|--S-C-E-N-E----[' + idx + ']--A-F-T-E-R--- ' + sleep + ' ms');				
 		setTimeout(() => {
 			scene.execute((err) => {				
-				// console.log('aaaaaaaaaaaa', err);
+				let sleep = 0;
 				if(idx === scenes.length -1) {
 					if(loop > 0 && --loop === 0) {
 						model.disconnect();
 						return;
 					}
+					sleep = scheduleConfig.sleep.all();
 					idx = 0;
 				}else{
+					sleep = scheduleConfig.sleep.each();
 					idx++;
 				}
-				scan(idx, loop);
+				scan(idx, loop, sleep);
 			});
 		}, sleep);
 	}	
 	setTimeout(()=>{
-		scan(0, loop);
+		scan(0, loop, 0);
 	}, 2000);	
 }
